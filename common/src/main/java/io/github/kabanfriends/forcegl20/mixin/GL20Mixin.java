e6b9739c573b74657746baaed44ec87fe2005914
KabanFriends <yuukun2004@protonmail.ch>
2023-05-23 06:51:39 +0900

    Fix ignore override

--- common/src/main/java/io/github/kabanfriends/forcegl20/mixin/GL20Mixin.java
+++ common/src/main/java/io/github/kabanfriends/forcegl20/mixin/GL20Mixin.java
@@ -2,6 +2,8 @@ package io.github.kabanfriends.forcegl20.mixin;
 
 import com.mojang.blaze3d.platform.Window;
 import io.github.kabanfriends.forcegl20.ForceGL20;
+import io.github.kabanfriends.forcegl20.override.HintOverride;
+import io.github.kabanfriends.forcegl20.override.OverrideType;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,9 +22,12 @@ public class GL20Mixin {
                 hintName = ForceGL20.GLFW_HINT_NAMES.get(hint);
             }
 
-            value = ForceGL20.GLFW_OVERRIDE_VALUES.get(hint).getValue();
-            ForceGL20.LOGGER.info("Overriding " + hintName + ": " + original + " -> " + (value == -2 ? "None" : value));
-            if (value == -2) {
+            HintOverride override = ForceGL20.GLFW_OVERRIDE_VALUES.get(hint);
+            OverrideType type = override.getOverrideType();
+            value = override.getValue();
+
+            ForceGL20.LOGGER.info("Overriding " + hintName + ": " + original + " -> " + (type == OverrideType.DO_NOT_SET ? "None" : value));
+            if (type == OverrideType.DO_NOT_SET) {
                 return;
             }
         }
